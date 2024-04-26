71616941a0c143901cb6ddf0d2efc46ae15a0327
Akhalid <86753011+akhalid07@users.noreply.github.com>
2024-04-26 10:57:19 -0400

    Update employee_reg.py

--- employee_reg.py
+++ employee_reg.py
@@ -53,7 +53,10 @@ class EmployeeRegistry:
 # Abstract base class for employee
 
 class Employee(ABC):
-    pass
+    def __init__(self, first_name, last_name, id_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id_number = id_number
 
 # Class NewEmployee definition
 class NewEmployee(Employee):
